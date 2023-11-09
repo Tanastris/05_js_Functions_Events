ee5914da3bafbc44067da152629cd15441116b22
RokasV <vezikauskas.r@gmail.com>
2023-11-09 09:48:37 +0200

    func

--- js/func.js
+++ js/func.js
@@ -24,3 +24,23 @@ sayHi("James");
 sayHi("Mike");
 sayHi();
 sayHi();
+
+// sukurti funkcija sumn() turetu sudeti 2 skaicius ir atspausdinti rezultata
+//paduoti skaicius kaip argumentus iskvieciant sum(5,10)
+
+function sum(vienas, du) {
+  const suma = vienas + du;
+  console.log("suma ===", suma);
+  return suma;
+}
+let rez1 = sum(6, 3);
+let rez2 = sum(4, 14);
+// atspausdinti ka grazina sityu sumu suma
+sum(rez1, rez2);
+
+function changeTitle(text = "document") {
+  document.title = text;
+  const h1El = document.querySelector("h1");
+  h1El.textContent = text;
+}
+changeTitle("namu darbai");
